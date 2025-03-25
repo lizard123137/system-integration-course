c0d97cde18c2d19fbea43627c974e9b1ca1796b1
lizard123137 <lizard123137@gmail.com>
2025-03-25 15:09:57 +0100

    filtering conflict data and chartjs integration

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,7 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, viewChild } from '@angular/core';
 import { CommoditiesService } from '../../services/commodities.service';
 import { AsyncPipe, JsonPipe, NgFor } from '@angular/common';
 import { ConflictsService } from '../../services/conflicts.service';
+import { ArcElement, BarController, BarElement, CategoryScale, Chart, ChartConfiguration, ChartOptions, Legend, LinearScale, LineController, LineElement, PointElement, TimeScale, Title, Tooltip } from 'chart.js';
+import { tap } from 'rxjs';
+import 'chartjs-adapter-date-fns';
+
+// Register necessary components for Chart.js
+Chart.register(
+  BarController,
+  BarElement,
+  LineController,
+  LinearScale,
+  CategoryScale,
+  TimeScale,
+  Title,
+  Tooltip,
+  Legend,
+  LineElement,
+  PointElement,
+  ArcElement
+);
+
 
 @Component({
   selector: 'app-home',
@@ -14,6 +34,11 @@ export class HomeComponent implements OnInit {
   commodities$: any;
   conflicts$: any;
 
+  conflicts: any;
+  conflicts_count: number = 0;
+
+  chart = viewChild<ElementRef>("chart");
+
   constructor(
     private commoditiesService: CommoditiesService,
     private conflictsService: ConflictsService,
@@ -21,6 +46,62 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     //this.commodities$ = this.commoditiesService.getData()
-    this.conflicts$ = this.conflictsService.getData();
+    this.conflicts$ = this.conflictsService
+      .getData()
+      .pipe(tap((data: any) => {
+        this.conflicts = data["data"];
+        this.conflicts_count = data["count"];
+        this.updateChart()
+    }));
+
+
+  }
+
+  updateChart() {
+    let chartData: any = {
+      datasets: [{
+        label: 'Conflicts Timeline',
+        data: this.conflicts.map((event: any) => ({
+          x: new Date(event["event_date"]),
+          y: 1,
+        })),
+      }]
+    };
+
+    let chartOptions: ChartOptions = {
+      plugins: {
+        title: {
+          text: 'Chart.js Time Scale',
+          display: true,
+        }
+      },
+      scales: {
+        x: {
+          type: 'time',
+          time: {
+            unit: 'day',
+            tooltipFormat: 'dd-mm-yyyy'
+          },
+          title: {
+            display: true,
+            text: 'Event date'
+          }
+        },
+        y: {
+          title: {
+            display: true,
+            text: 'value',
+          }
+        }
+      }
+    };
+    
+    let chartConfig: ChartConfiguration = {
+      type: 'line',
+      data: chartData,
+      options: chartOptions
+    };
+
+    new Chart(this.chart()?.nativeElement, chartConfig)
   }
 }
--- src/app/services/conflicts.service.ts
+++ src/app/services/conflicts.service.ts
@@ -11,6 +11,9 @@ export class ConflictsService {
   constructor(private http: HttpClient) { }
 
   getData(): Observable<any> {
-    return this.http.get(`${Environment.conflictsUrl}?key=${Environment.conflictsKey}&email=${Environment.conflictsEmail}`);
+    // inter = 1 means that we are looking for state sponsored forces
+    // For now use only europe
+    // More info about filtering at https://developer.acleddata.com/rehd/cms/views/acled_api/documents/API-User-Guide.pdf
+    return this.http.get(`${Environment.conflictsUrl}?key=${Environment.conflictsKey}&email=${Environment.conflictsEmail}&inter1=1&inter2=1&region=12`);
   }
 }
